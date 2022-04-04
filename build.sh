75f9ad117537289d4b09bf9a11f93076bc170a47
Luigi Rosso <luigi.rosso@gmail.com>
2022-04-04 16:04:42 -0700

    See rive.make

--- build.sh
+++ build.sh
@@ -37,8 +37,10 @@ else
             make clean
             make clean config=release
         elif [ "$OPTION" = "release" ]; then
+            cat rive.make
             make config=release -j7
         else
+            cat rive.make
             make -j7
         fi
     }
