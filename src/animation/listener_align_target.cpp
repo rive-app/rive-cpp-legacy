b5c67f1d4c87ce270f285cc4ab61430e95978936
Luigi Rosso <luigi.rosso@gmail.com>
2022-06-18 16:59:36 -0700

    Support for align action.

---
+++ src/animation/listener_align_target.cpp
@@ -0,0 +1,26 @@
+#include "rive/animation/listener_align_target.hpp"
+#include "rive/animation/state_machine_instance.hpp"
+// #include "rive/animation/state_machine_number.hpp"
+// #include "rive/animation/state_machine_input_instance.hpp"
+#include "rive/node.hpp"
+#include "rive/constraints/constraint.hpp"
+
+using namespace rive;
+
+void ListenerAlignTarget::perform(StateMachineInstance* stateMachineInstance,
+                                  Vec2D position) const {
+    auto coreTarget = stateMachineInstance->artboard()->resolve(targetId());
+    if (coreTarget == nullptr || !coreTarget->is<Node>()) {
+        return;
+    }
+    auto target = coreTarget->as<Node>();
+    Mat2D targetParentWorld = getParentWorld(*target);
+    Mat2D inverse;
+    if (!targetParentWorld.invert(&inverse)) {
+        return;
+    }
+
+    auto localPosition = inverse * position;
+    target->x(localPosition.x);
+    target->y(localPosition.y);
+}
